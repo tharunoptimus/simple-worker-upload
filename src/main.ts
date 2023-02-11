64eb4b89714ffe62135ed53abfd6958f538bd101
tharunoptimus <tharunoptimus@outlook.com>
2023-02-11 20:17:11 +0530

    Added pickAndSendFile as event handler for upload btn

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,29 @@ let selectedFileName: HTMLSpanElement | any =
 let statusSpan: HTMLSpanElement | any = document.querySelector(".status")
 let progressBar: HTMLProgressElement | any = document.querySelector(".progress")
 
+// Add event listener to the buttons
+uploadButton?.addEventListener("click", pickAndSendFile)
+
+function pickAndSendFile() {
+	let fileInput = document.createElement("input")
+	fileInput.type = "file"
+	fileInput.onchange = async (e) => {
+		fileUpload?.classList.remove("hidden")
+
+		let file = (e.target as HTMLInputElement).files![0]
+
+		let worker = new Worker(
+			new URL("./worker/upload.ts", import.meta.url),
+			{
+				type: "module",
+			}
+		)
+
+		worker.postMessage(file)
+	}
+
+	fileInput.click()
+}
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
